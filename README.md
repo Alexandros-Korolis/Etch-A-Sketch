672b1842a1080afc6f258bacb9397773a0e11078
alexandros <alex.korolis@gmail.com>
2023-03-25 19:40:10 +0100

    added button, js eventListeners and css modified

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is a browser version of something between a sketchpad and an Etch-A-Sketch. Using html/css and Javascript .
+
+1)Create a webpage with a 16x16 grid of square divs.
+2)Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
+3)It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
+4)You need make the divs appear as a grid (versus just one on each line).
+5)Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+6)Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
--- javascript.js
+++ javascript.js
@@ -1,13 +1,33 @@
-const containers = document.querySelectorAll('.container');
-containers.forEach((container) => {
-    for(let i=1; i<=16; i++){
-    const div = document.createElement('div');
-    container.appendChild(div);
+const box = document.querySelector('.box');
+let btn = document.querySelector('.btn');
+let btnClear = document.querySelector('.btnClear');
 
-    div.addEventListener('mouseover', function(e){
-        e.target.style.background="red";
-    })
-    
+btn.addEventListener("click", () => {
+
+    while(box.hasChildNodes()){
+        box.removeChild(box.firstChild);
     }
-})
 
+    let StringNumberPerSide = prompt("Please enter the number of squares in one side ");
+    let numberPerSide = parseInt(StringNumberPerSide);
+
+    if(numberPerSide>64 || numberPerSide < 2 || typeof numberPerSide != "number")
+    {
+        numberPerSide = 0;
+        alert("Wrong input !!");
+    }
+
+    for(let j = 1; j<=numberPerSide; j++){
+        const container  = document.createElement('div');
+        container.classList.add('container');
+        box.appendChild(container);
+
+        for(let i = 1; i<=numberPerSide; i++){
+            const div = document.createElement('div');
+            container.appendChild(div);
+            div.addEventListener('mouseover',function(e){
+                e.target.style.background="red";
+            });
+        }
+    }
+});
